d5dce214cd04c04f6e0904ca5f782dfd291699f9
Zaul Tavangar <zaul_tavangar@brown.edu>
2023-05-05 20:06:21 -0400

    plotting word map + cos sim

--- embedding_neighbors.py
+++ embedding_neighbors.py
@@ -10,20 +10,26 @@ def find_similar(vector_matrix, input_vector, num_examples):
     Use a basic similarity calculation (cosine similarity) to find
     the closest words to an input vector.
     """
+    print('input vector', input_vector[0].shape)
+
     # compute cosine similarity of input_vector with everything else in our vocabulary 
     cosine_similarities = linear_kernel(input_vector, vector_matrix).flatten()
     cosine_similarities /= np.linalg.norm(vector_matrix, axis=1)
 
+    # print('cosine sims shape: ', cosine_similarities.shape)
+
     # sort by cosine similarities, to get the most similar vectors on top
     related_words_indices = [i for i in cosine_similarities.argsort()[::-1]]
+
     return [index for index in related_words_indices][:num_examples]
 
 
 def find_similar_words(word_of_interest, year, num_examples=10):
-    procrustes_matrix = np.load('vectors/embeddings_rotated.npy') # shape: years, vocab_size_embedding_size
-    #print('procrustes matrix shape: ', procrustes_matrix.shape) # shape is (4095, 256, 19)
-    procrustes_matrix = np.transpose(procrustes_matrix, (2, 0, 1)) # convert shape to 19, 4096, 256
-    #print('procrustes matrix shape: ', procrustes_matrix.shape) # shape is (19, 4096, 256)
+    procrustes_matrix = np.load('vectors/embeddings_rotated.npy') # shape: years, vocab_size, embedding_size
+    print('procrustes matrix shape: ', procrustes_matrix.shape) # shape is (19, 4096, 256)
+
+    procrustes_matrix = np.transpose(procrustes_matrix, (2, 0, 1))
+    print('procrustes matrix shape: ', procrustes_matrix.shape) # shape is (19, 4096, 256)
 
     year_file = f'vectors/{year}_vectors.tsv'
     embedding_mat = np.loadtxt(year_file)
@@ -35,22 +41,30 @@ def find_similar_words(word_of_interest, year, num_examples=10):
 
     #print('shape word_mat: ', word_mat.shape)
 
-    index_of_interest = np.where(word_mat == word_of_interest)[0][0]
+    index_of_interest = np.where(word_mat == word_of_interest)[0]
+    if index_of_interest.size>0:
+        index_of_interest = index_of_interest[0]
+    else:
+        return None
     print(index_of_interest)
 
     similar_indices = find_similar(embedding_mat, [embedding_mat[index_of_interest]], num_examples)
-    embedding_mat_rotated = procrustes_matrix[year-2003]
+    embedding_mat_rotated = procrustes_matrix[int(year)-2003]
 
     similar_indices_rotated = find_similar(embedding_mat_rotated, [embedding_mat_rotated[index_of_interest]], num_examples) # rotated
 
+    words_to_indices = {}
     print("Words similar to", word_mat[index_of_interest], "in", year, '(regular embeddings)')
     for i in similar_indices:
-        print(' -', word_mat[i])
+        word = word_mat[i]
+        words_to_indices[word] = i
+        print(' -', word)
 
     print("Words similar to", word_mat[index_of_interest], "in", year, '(rotated embeddings)')
     for i in similar_indices_rotated:
         print(' -', word_mat[i])
-  
+
+    return words_to_indices
 
 find_similar_words("epstein", 2020)
 
--- word_analysis.py
+++ word_analysis.py
@@ -3,10 +3,15 @@ import numpy as np
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.widgets import Button
+from adjustText import adjust_text
+from sklearn.metrics.pairwise import cosine_similarity
+
+years = list(range(2003, 2022))
 
 def find_word_frequencies():
     dfs = []
-    years = list(range(2003, 2022))
 
     for year in years:
         #print(year)
@@ -28,36 +33,37 @@ def find_word_frequencies():
     print('Number of words which appear 19 times: ', count_19)
     print(df_counts.head(10))
 
+    return df_counts
+
     # 1777 words appear 19 times, 
     # Let's look at some of the most frequent AND interesting words from each year, using the year_metadata.tsv file, and see if they appear 19 times
       # If they do, they are candidates for plotting/further analysis
 
     # Words: police, govt, iraq/iraqi, fire, water, court, death, australia, war, attack, drug, election, trade
 
-    print(df_counts[df_counts['word'] == 'us']['count'].values[0])
+def find_word_frequency(word_of_interest):
+    df_counts = find_word_frequencies()
+    return (df_counts[df_counts['word'] == word_of_interest]['count'].values[0])
+
+def plot_nearest_neighbors(word_of_interest, year, num_examples):
 
 
+    print(year)
+    words_to_indices = embedding_neighbors.find_similar_words(word_of_interest, year, num_examples) # get dictionary of words to indices
+    if words_to_indices is None:
+        print(f'{word_of_interest} not found for {year}')
+        return
+    # words_to_indices.update(get_neighbors_of_neighbors(words_to_indices, 5))
 
-def plot_word_movement():
-    procrustes_matrix = np.load('vectors/embeddings_rotated.npy') # shape: years, vocab_size_embedding_size
-    procrustes_matrix = np.transpose(procrustes_matrix, (2, 0, 1)) # convert shape to 19, 4096, 256
-
-    years = list(range(2003, 2022))
-
-
-    pca = PCA(n_components=2)
-
-    embeddings_2d = []
-
-    # for year in years:
+    print(words_to_indices)
 
     labels=[]
     tokens=[]
 
-    # year_meta_file = f'vectors/{year}_metadata.tsv'
+    year_file = f'vectors/{year}_vectors.tsv'
+    embedding_mat = np.loadtxt(year_file)
 
-    embedding_mat = procrustes_matrix[0] # i.e. 2003
-    year_meta_file = f'vectors/2003_metadata.tsv'
+    year_meta_file = f'vectors/{year}_metadata.tsv'
     word_mat = np.loadtxt(year_meta_file, dtype=str)
 
     for word in word_mat:
@@ -65,56 +71,174 @@ def plot_word_movement():
         tokens.append(embedding_mat[index_of_interest])
         labels.append(word)
 
-    print(np.array(tokens).shape)
-    print(np.array(labels).shape)
+    # for rs in range(1, 51):
+    tsne_model = TSNE(n_components=2, random_state=9) #30 is good
+    reduced_vectors = tsne_model.fit_transform(np.array(tokens))
 
-    tsne_model = TSNE(n_components=2, perplexity=30, init='pca', n_iter=2500, random_state=42)
-    new_values = tsne_model.fit_transform(np.array(tokens))
+    specific_indices = np.array(list(words_to_indices.values()))
+    reduced_vectors = reduced_vectors[specific_indices, :]
+    relevant_words = [labels[index] for index in specific_indices]
 
-    x = []
-    y =[]
-    for value in new_values:
-        x.append(value[0])
-        y.append(value[1])
+    plt.figure(figsize=(15, 8))
 
-    plt.figure(figsize=(16, 16))
-    for i in range(len(x)):
-        plt.scatter(x[i], y[i])
-        plt.annotate(labels[i], 
-          xy=(x[i], y[i]),
-          xytext=(5,2),
-          textcoords='offset points',
-          ha = 'right',
-          va = 'bottom')
-        
+    # Assign a color index to each point based on its distance from the point of interest
+    distances = np.linalg.norm(reduced_vectors - reduced_vectors[0], axis=1)
+    max_distance = np.max(distances)
+    color_index = distances / max_distance
+    plt.scatter(reduced_vectors[1:, 0], reduced_vectors[1:, 1], 
+                c=color_index[1:], cmap='viridis', marker='o', s=50)
+    plt.scatter(reduced_vectors[0, 0], reduced_vectors[0, 1], 
+                c='r', marker='*', s=100)
+    
+    texts = []
+
+    for i, label in enumerate(relevant_words):
+        texts.append(plt.text(reduced_vectors[i, 0], reduced_vectors[i, 1], label))
+
+    adjust_text(texts, arrowprops=dict(arrowstyle='->', color='black')) # adjust labels so that they're read 
+
+    plt.title(f'Nearest of Neighbors for {word_of_interest}, {year}')
     plt.show()
 
+def get_vector_matrices(year, word1, word2):
+    procrustes_mat = np.load('vectors/embeddings_rotated.npy') # shape: years, vocab_size, embedding_size
+    rotated_embeddings = np.transpose(procrustes_mat, (2, 0, 1))
 
+    year_meta_file = f'vectors/{year}_metadata.tsv'
+    word_mat = np.loadtxt(year_meta_file, dtype=str)
 
-    # embedding_mat = procrustes_matrix[year-2003]
-    # #embedding_mat_2d = pca.fit_transform(embedding_mat)
-    # embedding_mat_2d = tsne.fit_transform(embedding_mat)
+    index1 = np.where(word_mat == word1)[0]
+    index2 = np.where(word_mat == word2)[0]
 
-    # print(embedding_mat_2d.shape)
-
-    # year_meta_file = f'vectors/{year}_metadata.tsv'
-    # word_mat = np.loadtxt(year_meta_file, dtype=str)
-
+    if index1.size>0 and index2.size>0:
+        index1 = index1[0] # get index of interest for word1
+        index2 = index2[0] # get index of interest for word2
+    else:
+        print('One of these words is not available')
+        return None, None
     
+    vector_word1 = rotated_embeddings[int(year)-2003][index1]
+    vector_word2 = rotated_embeddings[int(year)-2003][index2]
 
-    # #word_embedding = embedding_mat[index_of_interest] # embedding for the word of interest
-    
-    # print(embedding_mat_2d[index_of_interest])
-    # embeddings_2d.append(embedding_mat_2d[index_of_interest])
+    return vector_word1, vector_word2
 
-    # print(embeddings_2d)
-    
-    # for i, year in enumerate(years):
-    #     plt.scatter(embeddings_2d[i][0], embeddings_2d[i][1], label=year)
-    # plt.legend()
-    # plt.show()
+def get_cosine_similarity(year, word1, word2):
 
-      
-find_word_frequencies()
-plot_word_movement()
+    vector_word1, vector_word2 = get_vector_matrices(year, word1, word2)
 
+    if vector_word1 is None or vector_word2 is None:
+        return None
+    else:
+        cos_sim = cosine_similarity(vector_word1.reshape(1,-1), vector_word2.reshape(1, -1)) # convert to shape 1, 256 and final cos_sim
+        return cos_sim[0][0]
+
+
+
+def plot_cos_similarities(word1, word2):
+    freq_word1 = find_word_frequency(word1)
+    freq_word2 = find_word_frequency(word2)
+    print()
+    print(freq_word1, freq_word2)
+    # if find_word_frequency(word1) == find_word_frequency(word2) == 19:
+    cos_sim_list = []
+    for year in years:
+        cos_sim = get_cosine_similarity(year, word1, word2)
+        cos_sim_list.append(cos_sim)
+
+    # plot year vs cosine similarity as a scatter plot
+    plt.figure(figsize=(15, 8))
+    plt.scatter(years, cos_sim_list)
+    plt.xlabel('Year')
+    plt.ylabel('Cosine Similarity')
+    plt.title(f'Cosine Similarity between {word1} and {word2}')
+    plt.xticks(years)
+    plt.grid(True) # show grid on both axes
+    plt.ylim(-0.5, 0.5)  # set y-axis limits
+
+    # exclude None values from the scatter plot
+    x = [years[i] for i in range(len(cos_sim_list)) if cos_sim_list[i] is not None]
+    y = [cos_sim_list[i] for i in range(len(cos_sim_list)) if cos_sim_list[i] is not None]
+    plt.scatter(x, y)
+
+    print(len(x))
+     # plot dashed lines between the points
+    for i in range(len(x)-1):
+        if not cos_sim_list[i+1] is None:
+            plt.plot([x[i], x[i+1]], [y[i], y[i+1]], '--', color='grey')
+
+    plt.show()
+
+    # else:
+    #     print(f'{word1} and {word2} do not appear in all the years from 2003-2021')
+
+
+find_word_frequencies() # getting word frequencies
+
+# set parameters of interest
+word_of_interest = 'epstein'
+year = '2020'
+num_examples = 10
+
+# find word freq 
+word_freq = find_word_frequency(word_of_interest)
+print(f'{word_of_interest} word frequency: {word_freq}')
+
+# # plot nearest neighbors
+# plot_nearest_neighbors(word_of_interest, year, num_examples)
+
+# plot cosine similarity
+plot_cos_similarities('violence', 'terrorist') 
+# vaccine, protest; president, election; president, approval; china, trade/export
+
+# plot cosine similarity 
+
+
+# 1, 4, 5, 9, 17, 
+# 21, 30
+
+# 31-50
+
+# def get_neighbors_of_neighbors(words_to_indices, num_examples):
+#     new_words_to_indices = {}
+#     for word in words_to_indices:
+#         neighboring_words_to_indices = embedding_neighbors.find_similar_words(word, year, num_examples=num_examples)
+#         for w, i in neighboring_words_to_indices.items():
+#             if w not in words_to_indices:
+#                 new_words_to_indices[w] = i
+#     return new_words_to_indices
+
+
+# for word, index in words_to_indices.items():
+#     if word not in tokens_set:
+#         tokens_set.add(word)
+#         tokens.append(embedding_mat[index])
+#         labels.append(word)
+#     neighboring_words_to_indices = embedding_neighbors.find_similar_words(word, year, num_examples=10) # find 10 neighbors of neighbor
+#     for w2, i2 in neighboring_words_to_indices.items():
+#         if w2 not in tokens_set:
+#             tokens_set.add(w2)
+#             tokens.append(embedding_mat[i2])
+#             labels.append(w2)
+
+# embedding_mat = procrustes_matrix[year-2003]
+# #embedding_mat_2d = pca.fit_transform(embedding_mat)
+# embedding_mat_2d = tsne.fit_transform(embedding_mat)
+
+# print(embedding_mat_2d.shape)
+
+# year_meta_file = f'vectors/{year}_metadata.tsv'
+# word_mat = np.loadtxt(year_meta_file, dtype=str)
+
+
+
+# #word_embedding = embedding_mat[index_of_interest] # embedding for the word of interest
+
+# print(embedding_mat_2d[index_of_interest])
+# embeddings_2d.append(embedding_mat_2d[index_of_interest])
+
+# print(embeddings_2d)
+
+# for i, year in enumerate(years):
+#     plt.scatter(embeddings_2d[i][0], embeddings_2d[i][1], label=year)
+# plt.legend()
+# plt.show()
